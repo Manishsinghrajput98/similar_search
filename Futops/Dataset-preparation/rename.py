d2c120e94a1d8fb7be81c93a5e7a1779fc7dae7a
Manish Rajput <manishsinghrajput199564@gmail.com>
2021-01-31 20:55:11 +0530

    Delete Futops directory

--- Futops/Dataset-preparation/rename.py
+++
@@ -1,9 +0,0 @@
-import os 
-def main():   
-    for count, filename in enumerate(os.listdir("/home/rajput/Desktop/Dataset_Preparation/xmls")): 
-        dst ="new" + str(count) + ".xml"
-        src ='/home/rajput/Desktop/Dataset_Preparation/xmls/'+ filename 
-        dst ='/home/rajput/Desktop/Dataset_Preparation/xmls/'+ dst 
-        os.rename(src, dst) 
-if __name__ == '__main__': 
-    main() 
